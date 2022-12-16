a0e1133f7fcdec4dd1223237f35b53b1141e3ffe
Ong Dion <djo38@zoo.cs.yale.edu>
2022-12-15 21:41:15 -0500

    fix ab agent

--- include/engines/ab_engine.hpp
+++ include/engines/ab_engine.hpp
@@ -9,7 +9,7 @@
 class AB_Engine : public Engine {
 public:
     long long evaluation(Board& board, int depth);
-    long long search(Board& board, long long alpha, long long beta, int depth, int turn);
+    long long search(Board& board, long long alpha, long long beta, int depth, int turn, int player);
 
 private:
     std::mutex map_mutex;
--- src/engines/ab_engine.cpp
+++ src/engines/ab_engine.cpp
@@ -2,17 +2,19 @@
 #include "board.hpp"
 #include "common.hpp"
 
+#include <climits>
 #include <iostream>
 #include <utility>
 
 long long AB_Engine::evaluation(Board& board, int depth) {
-    return search(board, -INF, INF, depth, board.count());
+    return search(board, -INF, INF, depth, board.count(), 1);
 }
 
-long long AB_Engine::search(Board& board, long long alpha, long long beta, int depth, int turn) {
+long long AB_Engine::search(Board& board, long long alpha, long long beta, int depth, int turn, int player) {
     {
         std::lock_guard<std::mutex> lk(map_mutex);
-        if (this->turn != turn) return 0;
+        if (this->turn != turn)
+            return player == 1 ? LONG_LONG_MIN : LONG_LONG_MAX;
         if (table[depth].count(board)) {
             auto [lower, upper] = table[depth][board];
             if (lower >= beta)
@@ -26,37 +28,65 @@ long long AB_Engine::search(Board& board, long long alpha, long long beta, int d
     
     if (board.is_terminal()) {
         int diff = board.count_player() - board.count_opponent();
-        return ((diff > 0) - (diff < 0)) * INF_EVAL;
+        return ((diff > 0) - (diff < 0)) * INF_EVAL * player;
     }
     if (depth == 0)
-        return (*heuristic_function)(board);
+        return (*heuristic_function)(board) * player;
     
-    long long val, a;
-    val = -INF, a = alpha;
+    long long val, a, b;
     uint64_t moves = board.get_moves();
-    if (moves == 0) {
-        Move move = board.do_move(-1);
-        val = std::max(val, -search(board, -beta, -a, depth, turn));
-        a = std::max(a, val);
-        board.undo_move(move);
+    if (player == 1) {
+        val = -INF, a = alpha;
+        if (moves == 0) {
+            Move move = board.do_move(-1);
+            val = std::max(val, search(board, a, beta, depth, turn, -player));
+            a = std::max(a, val);
+            board.undo_move(move);
+        }
+        else {
+            for (; moves > 0 && val < beta; moves -= moves & (-moves)) {
+                Move move = board.do_move(__builtin_ctzll(moves));
+                val = std::max(val, search(board, a, beta, depth - 1, turn, -player));
+                a = std::max(a, val);
+                board.undo_move(move);
+            }
+        }
     }
-    for (; moves > 0 && val < beta; moves -= moves & (-moves)) {
-        Move move = board.do_move(__builtin_ctzll(moves));
-        val = std::max(val, -search(board, -beta, -a, depth - 1, turn));
-        a = std::max(a, val);
-        board.undo_move(move);
+    else {
+        val = INF, b = beta;
+        if (moves == 0) {
+            Move move = board.do_move(-1);
+            val = std::min(val, search(board, alpha, b, depth, turn, -player));
+            b = std::min(b, val);
+            board.undo_move(move);
+        }
+        else {
+            for (; moves > 0 && val > alpha; moves -= moves & (-moves)) {
+                Move move = board.do_move(__builtin_ctzll(moves));
+                val = std::min(val, search(board, alpha, b, depth - 1, turn, -player));
+                b = std::min(b, val);
+                board.undo_move(move);
+            }
+        }
     }
     {
         std::lock_guard<std::mutex> lk(map_mutex);
-        if (val <= alpha) {
-            table[depth][board].second = val;
-        }
-        if (val > alpha && val < beta) {
-            table[depth][board].second = val;
-            table[depth][board].first = val;
-        }
-        if (val >= beta) {
-            table[depth][board].first = val;
+        if (val != LONG_LONG_MAX && val != LONG_LONG_MIN) {
+            bool exists = table[depth].count(board);
+            if (val <= alpha) {
+                table[depth][board].second = val;
+                if (!exists)
+                    table[depth][board].first = -INF;
+            }
+            if (val > alpha && val < beta) {
+                table[depth][board].second = val;
+                table[depth][board].first = val;
+            }
+            if (val >= beta) {
+                table[depth][board].first = val;
+                if (!exists)
+                    table[depth][board].second = INF;
+            }
         }
     }
     return val;
